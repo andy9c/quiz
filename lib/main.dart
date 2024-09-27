16d43031271e83ddc7a1e8c9225be7638c3304cd
Anand Tirkey (STPS Office) <andy9c@gmail.com>
2024-09-27 16:32:41 +0530

    major fix (leaderboard)

--- lib/main.dart
+++ lib/main.dart
@@ -255,7 +255,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       itemCount: sortedData.entries.toList().length,
                       itemBuilder: (BuildContext context, int index) {
                         String key =
-                            snapshot.data!.keys.elementAt(index).split(';')[0];
+                            sortedData.keys.elementAt(index).split(';')[0];
                         return Column(
                           children: <Widget>[
                             ListTile(
